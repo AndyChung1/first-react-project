39cd5ac8f234abea947a34d1fab696d51b4dadfb
Andy <chungster239@gmail.com>
2023-08-25 00:30:35 -0400

    Add subtotal

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,6 +7,16 @@ export default function Cart() {
   const { cartItems } = useContext(ShopContext);
 
   let cart = Object.entries(cartItems);
+  const getSubtotal = (cartItems) => {
+    let subTotal = 0
+    let cart = Object.entries(cartItems)
+    for (let i of cart) {
+        const product = getProductFromId(Number(i[0]))
+        subTotal += product.price * i[1]
+    }
+    return Math.round(subTotal * 100) / 100
+}
+
   const cartQuantity = (cart) => {
     let res = 0;
     for (let i of cart) {
@@ -14,7 +24,17 @@ export default function Cart() {
     }
     return res;
   };
+
+  const getProductFromId = (id) => {
+    for (let product of laptops) {
+        if (product.id === id) {
+            return product
+        }
+    }
+  }
+
   let quantity = cartQuantity(cart);
+  let subTotal = getSubtotal(cartItems);
   return (
     <div>
       <h1>{quantity > 0 ? "Your Cart Items" : "Your cart is empty"}</h1>
@@ -25,10 +45,12 @@ export default function Cart() {
               key={product.id}
               product={product}
               quantity={cartItems[product.id]}
+              subTotal={subTotal}
             />
           );
         }
       })}
+      <p>{quantity > 0 && `Subtotal: $${subTotal}`}</p>
     </div>
   );
 }
